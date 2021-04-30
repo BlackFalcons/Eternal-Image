22ee7a877867f40a4f5f60e4a2db850ec72b34de
Eivind Limstrand <brage211@gmail.com>
2021-04-30 15:30:36 +0200

    Replace bad variable name and remove TODO message

--- src/com/falcon/Main.java
+++ src/com/falcon/Main.java
@@ -8,13 +8,12 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //TODO Remove usage of absolute path on all local files.
-        BufferedImage source = ImageIO.read(GetFile("images\\equipment-tab.png"));
+        BufferedImage MainImage = ImageIO.read(GetFile("images\\equipment-tab.png"));
         BufferedImage item = ImageIO.read(GetFile("images\\22484.png"));
 
-        AddToImage(source, item, 30, 91);
-        AddToImage(source, item, 70, 91);
-        SaveImage(source, "png", GetFile("images\\output_image.png"));
+        AddToImage(MainImage, item, 30, 91);
+        AddToImage(MainImage, item, 70, 91);
+        SaveImage(MainImage, "png", GetFile("images\\output_image.png"));
     }
 
     public static File GetFile(String fileName) {
